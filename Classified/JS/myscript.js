b2decbcd01265901b702d86dc99e3655d4054156
Ahmer Ali Ahsan <ahmer.ali.ahsan@outlook.com>
2016-09-21 23:23:46 +0500

    Add files via upload

---
+++ Classified/JS/myscript.js
@@ -0,0 +1,5 @@
+/// <reference path="../typings/index.d.ts" />
+$(document).ready(function () {
+    alert('Is document ready?');
+});
+//# sourceMappingURL=myscript.js.map
---
+++ Classified/TS/myscript.ts
@@ -0,0 +1,4 @@
+/// <reference path="../typings/index.d.ts" />
+$(document).ready(function(){
+    alert('Is document ready?');
+});
